f1b59ca8eb0d213c7652c1c7738bc3ea4799d5bc
Larry Williamson <l422y@l422y.com>
2024-03-26 13:53:01 -0400

    feat: initialize Nuxt 3 project with Pinia, docs, and basic setup

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{testStore.count}}
+    <button @click="testStore.increment">
+      Increment
+    </button>
+    <button @click="testStore.decrement">
+      Decrement
+    </button>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useTestStore } from "~/stores/test"
+
+const testStore = useTestStore()
+
+</script>
