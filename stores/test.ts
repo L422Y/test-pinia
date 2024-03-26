e2382cf925ef197597cee233f9e81e4a4718ea92
Larry Williamson <l422y@l422y.com>
2024-03-26 13:52:38 -0400

    feat(stores): add useTestStore with increment and decrement functions

---
+++ stores/test.ts
@@ -0,0 +1,6 @@
+export const useTestStore = defineStore("test", () => {
+    const count = ref(0)
+    const increment = () => count.value++
+    const decrement = () => count.value--
+    return {count, increment, decrement}
+})
